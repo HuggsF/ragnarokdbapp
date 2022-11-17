171f7420d1a02f69dd2e5b7a133080f1dbf10d2e
HuggsF <tsugamebashi@gmail.com>
2022-11-17 19:22:10 -0300

    few more archives on first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
@@ -7,16 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          RagnarokdatabaseApp
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/Pages/login/index.jsx
@@ -0,0 +1,7 @@
+const Login = () =>{
+    return (<>
+        <h1>Login</h1>
+        <button>Voltar para Home</button>
+    </>)
+}
+export { Login }
